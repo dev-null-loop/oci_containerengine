766d1c04280d2c4ebeabb7f0a11a1ae1746cc166
dev-null-loop <dev.null.loop@gmail.com>
2024-11-18 14:00:53 +0200

    addon: 1st version

---
+++ addon/main.tf
@@ -0,0 +1,14 @@
+resource "oci_containerengine_addon" "this" {
+  addon_name                       = var.addon_name
+  cluster_id                       = var.cluster_id
+  remove_addon_resources_on_delete = var.remove_addon_resources_on_delete
+  dynamic "configurations" {
+    for_each = var.configurations[*]
+    content {
+      key   = configurations.value.key
+      value = configurations.value.value
+    }
+  }
+  override_existing = var.override_existing
+  version           = var.addon_version
+}
---
+++ addon/variables.tf
@@ -0,0 +1,36 @@
+variable "addon_name" {
+  description = "(Required) The name of the addon."
+  type        = string
+}
+
+variable "cluster_id" {
+  description = "(Required) The OCID of the cluster."
+  type        = string
+}
+
+variable "remove_addon_resources_on_delete" {
+  description = "(Required) Whether to remove addon resource in deletion."
+  type        = bool
+  default     = true
+}
+
+variable "configurations" {
+  description = "(Optional) (Updatable) Addon configuration details"
+  type = object({
+    key   = optional(string)
+    value = optional(string)
+  })
+  default = null
+}
+
+variable "override_existing" {
+  description = "(Optional) Whether or not to override an existing addon installation. Defaults to false. If set to true, any existing addon installation would be overridden as per new installation details."
+  type        = bool
+  default     = false
+}
+
+variable "addon_version" {
+  description = "(Optional) (Updatable) The version of addon to be installed."
+  type        = number
+  default     = 1
+}
