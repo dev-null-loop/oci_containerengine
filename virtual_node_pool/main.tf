fad3e100d3fd4b11a2553021208f8a16ae3a7b9f
dev-null-loop <dev.null.loop@gmail.com>
2025-03-02 20:24:22 +0200

    containerengine:virtual_pool:refactoring

--- virtual_node_pool/main.tf
+++ virtual_node_pool/main.tf
@@ -10,20 +10,6 @@ resource "oci_containerengine_virtual_node_pool" "this" {
   cluster_id     = var.cluster_id
   compartment_id = var.compartment_id
   display_name   = var.display_name
-  defined_tags   = var.defined_tags
-  freeform_tags  = var.freeform_tags
-  nsg_ids        = var.nsg_ids
-  size           = var.size
-
-  dynamic "initial_virtual_node_labels" {
-    for_each = var.initial_virtual_node_labels[*]
-    iterator = ivnl
-    content {
-      key   = ivnl.value.key
-      value = ivnl.value.value
-    }
-  }
-
   dynamic "placement_configurations" {
     for_each = var.placement_configurations
     iterator = pc
@@ -33,13 +19,23 @@ resource "oci_containerengine_virtual_node_pool" "this" {
       subnet_id           = var.subnet_ids[pc.value.subnet_name]
     }
   }
-
+  defined_tags  = var.defined_tags
+  freeform_tags = var.freeform_tags
+  dynamic "initial_virtual_node_labels" {
+    for_each = var.initial_virtual_node_labels[*]
+    iterator = ivnl
+    content {
+      key   = ivnl.value.key
+      value = ivnl.value.value
+    }
+  }
+  nsg_ids = var.nsg_ids
   pod_configuration {
     shape     = var.pod_configuration.shape
     subnet_id = var.subnet_ids[var.pod_configuration.subnet_name]
     nsg_ids   = var.pod_configuration.nsg_ids
   }
-
+  size = var.size
   dynamic "taints" {
     for_each = var.taints[*]
     iterator = t
@@ -49,7 +45,6 @@ resource "oci_containerengine_virtual_node_pool" "this" {
       value  = t.value.value
     }
   }
-
   dynamic "virtual_node_tags" {
     for_each = var.virtual_node_tags[*]
     iterator = vnt
