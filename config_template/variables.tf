61b40c9efc81a6161e9a5ba2a18ef7a0bde04352
dev-null-loop <dev.null.loop@gmail.com>
2025-03-26 11:29:48 +0200

    containerengine:typo

--- config_template/variables.tf
+++ config_template/variables.tf
@@ -1,5 +1,5 @@
 variable "cluster_id" {
-  description = "OCID of the cluster to which this node pool is attached"
+  description = "OCID of the OKE cluster"
   type        = string
 }
 
@@ -10,12 +10,12 @@ variable "cluster_name" {
 
 variable "oci_profile" {
   description = "OCI config profile"
-  default     = "DEFAULT"
   type        = string
+  default     = "DEFAULT"
 }
 
 variable "helm_config_dir" {
   description = "Path where OKE config files will be saved"
-  default     = "./"
   type        = string
+  default     = "./"
 }
