6b7a3218f2cdf3ead2a2e134ae7b73f6ff8b25fe
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-12 11:14:48 +0300

    config_template: +instance_principal

--- config_template/variables.tf
+++ config_template/variables.tf
@@ -1,27 +1,34 @@
 variable "cluster_id" {
-  description = "OCID of the OKE cluster"
+  description = "(Required) The OCID of the cluster."
   type        = string
 }
 
 variable "cluster_name" {
-  description = "cluster name key"
+  description = "(Required) Cluster name"
   type        = string
 }
 
 variable "oci_profile" {
-  description = "OCI config profile"
+  description = "(Optional) OCI config profile"
   type        = string
   default     = "DEFAULT"
 }
 
 variable "helm_config_dir" {
-  description = "Path where OKE config files will be saved"
+  description = "(Optional) Path where OKE config files will be saved"
   type        = string
   default     = "."
 }
 
 variable "providers_enabled" {
-  description = "Create helm/kubernetes provider configuration file or not"
+  description = "(Optional) Wheter to generate helm/kubernetes provider configuration file or not"
+  type        = bool
+  default     = false
+}
+
+
+variable "instance_principal_enabled" {
+  description = "(Optional) Whether to generate kubeconfig for an instance principal or not."
   type        = bool
   default     = false
 }
