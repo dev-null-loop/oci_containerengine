a063b84706c6e4bd53538c1b2755999798e709f2
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-14 12:53:31 +0300

    cluster: +service_lb_subnet_ids output

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "The OCID of the cluster."
   value       = oci_containerengine_cluster.this.id
 }
+
+output "service_lb_subnet_ids" {
+  description = "The OCIDs of the subnets used for Kubernetes services load balancers."
+  value       = oci_containerengine_cluster.this.service_lb_subnet_ids
+}
