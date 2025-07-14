b70467fe4dbfc539f7d4d3890e407fc1426a50e4
bogdan-m-darie <bogdan.m.darie@oracle.com>
2025-07-14 13:53:49 +0300

    cluster: fix no 3 for output service_lb_subnet_ids

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -5,5 +5,5 @@ output "id" {
 
 output "service_lb_subnet_ids" {
   description = "The OCIDs of the subnets used for Kubernetes services load balancers."
-  value       = [for i in oci_containerengine_cluster.this.options : i.service_lb_subnet_ids]
+  value       = oci_containerengine_cluster.this.options[0].service_lb_subnet_ids
 }
