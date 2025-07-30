14c59810dc583eabf6fe4ca6c60f6ab30831137e
dev-null-loop <dev.null.loop@gmail.com>
2025-07-30 11:30:07 +0300

    node_pool: +nodes output

--- node_pool/outputs.tf
+++ node_pool/outputs.tf
@@ -1,3 +1,8 @@
 output "id" {
   value = oci_containerengine_node_pool.this.id
 }
+
+output "nodes" {
+  description = "The nodes in the node pool"
+  value       = oci_containerengine_node_pool.this.nodes
+}
