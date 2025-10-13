6d95ebafb284f803d7757907551f7a722bd62ccb
dev-null-loop <dev.null.loop@gmail.com>
2025-10-13 14:18:30 +0300

    cluster: +extra outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -3,31 +3,17 @@ output "id" {
   value       = oci_containerengine_cluster.this.id
 }
 
+output "endpoints" {
+  description = "Endpoints served up by the cluster masters."
+  value       = oci_containerengine_cluster.this.endpoints
+}
+
+output "kubernetes_network_config" {
+  description = "Network configuration for Kubernetes."
+  value       = oci_containerengine_cluster.this.options[0].kubernetes_network_config
+}
+
 output "service_lb_subnet_ids" {
   description = "The OCIDs of the subnets used for Kubernetes services load balancers."
   value       = oci_containerengine_cluster.this.options[0].service_lb_subnet_ids
 }
-
-# options {
-#   ip_families           = ["IPv4", ]
-#   service_lb_subnet_ids = ["ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaaa255dy2jwu6ufxhfgosrvnelhj46hpnzpv25ceuruyjgtjm2ze4q", ]
-#   add_ons {
-#     is_kubernetes_dashboard_enabled = false
-#     is_tiller_enabled               = false
-#   }
-#   admission_controller_options {
-#     is_pod_security_policy_enabled = false
-#   }
-#   kubernetes_network_config {
-#     pods_cidr     = "10.244.0.0/16"
-#     services_cidr = "10.96.0.0/16"
-#   }
-#   persistent_volume_config {
-#     defined_tags  = {}
-#     freeform_tags = {}
-#   }
-#   service_lb_config {
-#     defined_tags  = {}
-#     freeform_tags = {}
-#   }
-# }
